343ed33504018d45621c4766a6cbd99c5e7b5723
KarinaKorsgaard <kkorsgaardjensen@gmail.com>
2018-03-11 11:52:51 +0100

    123 fixed

---
+++ src/100_looserTick.h
@@ -0,0 +1,13 @@
+//
+//  100_looserTick.h
+//  _ETK_March_2017
+//
+//  Created by Karina Jensen on 10/03/2018.
+//
+//
+
+#ifndef _00_looserTick_h
+#define _00_looserTick_h
+
+
+#endif /* _00_looserTick_h */
